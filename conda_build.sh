e36918be351dc8a0793709cc0ddb09609694b28a
kaihsinwu <kaihsinwu@gmail.com>
2020-06-12 10:28:55 -0400

    upload

--- conda_build.sh
+++ conda_build.sh
@@ -1,5 +1,5 @@
 conda config --set anaconda_upload no
 
 OUTPUT_FN=$(conda-build cytnx_37dev/ --output)
-conda-build cytnx_37dev/
+conda-build conda_37dev/
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u kaihsinwu -l nightly $OUTPUT_FN --force
