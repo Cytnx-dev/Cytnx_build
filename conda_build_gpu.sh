117ea8391a150599a5ab8511abbbae3e244acf4f
kaihsinwu <kaihsinwu@gmail.com>
2020-06-12 11:20:07 -0400

    [Update] Finalize upload

---
+++ conda_build_gpu.sh
@@ -0,0 +1,11 @@
+conda config --set anaconda_upload no
+
+if [ $TOXENV = 'py36' ]; then
+    OUTPUT_FN=$(conda-build conda_gpu_36/ --output)
+    conda-build conda_gpu_36
+else
+    OUTPUT_FN=$(conda-build conda_gpu_37/ --output)
+    conda-build conda_gpu_37
+fi
+
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u kaihsinwu $OUTPUT_FN --force
